128451b6d6c105335fe9d02c49225b4213702d2e
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-28 22:01:10 +0000

    setup for shopping list

--- src/index.js
+++ src/index.js
@@ -9,6 +9,24 @@ class App extends Component {
 				<div className="jumbotron text-center">
 					<h1 className="display-4">Shopping List</h1>
 				</div>
+				<ul className="list-group text-center">
+					<li className="list-group-item">Item</li>
+					<li className="list-group-item">Item</li>
+					<li className="list-group-item">Item</li>
+					<li className="list-group-item">Item</li>
+					<li className="list-group-item">Item</li>
+				</ul>
+				<hr />
+				<form>
+					<div className="form-group">
+						<input type="text" name="input" className="form-control" />
+					</div>
+					<div className="form-group">
+						<button type="button" className="btn btn-primary btn-lg btn-block">
+							Add New Item
+						</button>
+					</div>
+				</form>
 			</div>
 		);
 	}
